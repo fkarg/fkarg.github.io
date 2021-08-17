79a67539a9b925cb91fe90a83037bc2e6a492d94
fkarg <f.karg10@gmail.com>
2021-08-17 21:33:04 +0200

    [analyze] two-step, with interaction

--- staged/analyse.py
+++ staged/analyse.py
@@ -5,5 +5,9 @@ from homer.cmdline_printer import ArticlePrinter
 
 article = Article('Post', 'pars', open(sys.argv[1]).read())
 pa = ArticlePrinter(article)
-pa.print_article_stats()
-pa.print_paragraph_stats()
+try:
+    pa.print_article_stats()
+    input("Ready to continue? [Enter]")
+    pa.print_paragraph_stats()
+except (KeyboardInterrupt, EOFError):
+    pass
