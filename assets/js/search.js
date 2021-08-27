cb373a932b89531f4103dc967a6db77f30dc2fb2
fkarg <f.karg10@gmail.com>
2021-08-27 16:05:08 +0200

    [search] adjust parameters

--- assets/js/search.js
+++ assets/js/search.js
@@ -7,8 +7,8 @@
       for (var i = 0; i < results.length; i++) {  // Iterate over the results
         var item = store[results[i].ref];
         appendString += '<article class="post-preview">'
-        appendString += '<a href="' + item.url + '"><h2 class="post-title">' + item.title + '</h2></a>';
-        appendString += '<h3 class="post-subtitle">' + item.subtitle + '</h3>';
+        appendString += '<a href="' + item.url + '"><h2 class="post-title">' + item.title + '</h2>';
+        appendString += '<h3 class="post-subtitle">' + item.subtitle + '</h3></a>';
         appendString += '<p class="post-meta"> Posted on ' + item.date +'</p>';
         if (item.thumbnail != "") {
             appendString += '<div class="post-entry-container"><div class="post-image"><a href="' + item.url + '"><img src="'+ item.thumbnail +'"></a></div>';
@@ -19,10 +19,9 @@
         appendString += '</article>';
       }
 
-
       searchResults.innerHTML = appendString;
     } else {
-      searchResults.innerHTML = 'Not found.';
+      searchResults.innerHTML = 'No post with these words found.';
     }
   }
 
@@ -48,24 +47,39 @@
     // a boost of 10 to indicate matches on this field are more important.
       var idx = lunr(function () {
             this.field('id');
+            this.field('url', { boost: 10 });
             this.field('title', { boost: 10 });
-            this.field('subtitle', { boost: 5});
+            this.field('tags', { boost: 5 });
+            this.field('subtitle', { boost: 5 });
+            this.field('post_entry', { boost: 3 });
             this.field('content');
             for (var key in window.store) { 
                 this.add({
                     'id': key,
-                    'date': window.store[key].date,
+                    // 'date': window.store[key].date,
                     'title': window.store[key].title,
                     'tags': window.store[key].tags,
-                    'thumbnail': window.store[key].thumbnail,
+                    'url': window.store[key].url,
+                    // 'thumbnail': window.store[key].thumbnail,
                     'subtitle': window.store[key].subtitle,
-                    'content': window.store[key].content,
-                    'post_entry': window.store[key].post_entry
+                    'post_entry': window.store[key].post_entry,
+                    'content': window.store[key].content
                 });
             }
         });
 
-        var results = idx.search('*' + searchTerm + '*'); // Get lunr to perform a search
+        searchTerm = searchTerm.split(' ').join('*~1 ');
+        // console.log("searching for: " + searchTerm);
+
+        results = '';
+        if (searchTerm.split(' ').length > 1) {
+            results = idx.search(searchTerm + '*~1'); // Get lunr to perform a search
+        }
+        else {
+            results1 = idx.search(searchTerm + '~1'); // Get lunr to perform a search
+            results2 = idx.search(searchTerm + '*~1'); // Get lunr to perform a search
+            results = results1.length > results2.length ? results1 : results2;
+        }
         displaySearchResults(results, window.store);
   }
 })();
